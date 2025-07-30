b2f68bf3e884edf8be422538729627aebdb9608f
«avidalhe» <u1978866@campus.udg.edu>
2025-07-30 19:10:56 +0200

    is_valid completed!

--- Easy/valid_parentheses/src/main.rs
+++ Easy/valid_parentheses/src/main.rs
@@ -35,17 +35,79 @@ s consists of parentheses only '()[]{}'.
 */
 
 struct Solution;
+
 impl Solution {
     pub fn is_valid(s: String) -> bool {
-        let resultat: bool = false;
-        for i in s.chars().into_iter(){
-            
+        // Variable que retornarà la funció
+        let mut resultat: bool = false;
+        // Vector per anar sabent l'ordre al qual s'estant obrint els parèntessis.
+        let mut tancament: Vec<i8> = vec![];
+
+        // Fem un bucle per a cada un dels caràcters
+        for value in s.chars(){
+            match value {
+                '(' => {
+                    tancament.push(1);
+                },
+                '[' => {
+                    tancament.push(2);
+                },
+                '{' => {
+                    tancament.push(3);
+                },
+                ')' => {
+                    if !tancament.is_empty() {
+                        if let Some(last) = tancament.last() {
+                            if *last == 1 {
+                                tancament.pop();
+                            } else {
+                                return false
+                            }
+                        }
+                    } else {
+                        return false;
+                    }
+                },
+                ']' => {
+                    if !tancament.is_empty() {
+                        if let Some(last) = tancament.last() {
+                            if *last == 2 {
+                                tancament.pop();
+                            } else {
+                                return false;
+                            }
+                        }
+                    } else {
+                        return false;
+                    }
+                },
+                '}' => {
+                    if !tancament.is_empty() {
+                        if let Some(last) = tancament.last() {
+                            // Com que last és una referència a la memòria i nosaltres volem desenpecatar aquesta referència per comprar posem el *
+                            if *last == 3 {
+                                tancament.pop();
+                            } else {
+                                return false;
+                            }
+                        }
+                    } else {
+                        return false;
+                    }
+                }
+                _ => (),
+            }
         }
+        // si tots els tipus són falsos vol dir que s'ha tancat correctament tots els parèntessis
+        if tancament.is_empty() {
+            resultat = true;
+        }
+        resultat
     }
 }
 
 fn main() {
-    if Solution::is_valid(String::from("({})")) {
+    if Solution::is_valid(String::from("[[[]")) {
         println!("Resultat = True");
     }else{
         println!("Resultat = False");
